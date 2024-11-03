1eb285adb7f721da2eaa79dcea03eef8de1c9759
siggyweb <100291941+siggyweb@users.noreply.github.com>
2024-11-03 00:32:41 +0000

    Added command based tick for command currying

--- internal/vgrep.go
+++ internal/vgrep.go
@@ -6,6 +6,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"golang.design/x/clipboard"
 	"os/exec"
+	"time"
 )
 
 // bubbletea application state model
@@ -35,7 +36,7 @@ func InitialModel() Model {
 }
 
 func (m Model) Init() tea.Cmd {
-	return textinput.Blink
+	return tickEvery()
 }
 
 func (m Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
@@ -51,13 +52,23 @@ func (m Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+q":
 			clipboard.Write(clipboard.FmtText, []byte(m.output))
 
-		// for testing purposes to see key input received
-		default:
-			m.output += msg.String()
+			// for testing purposes to see key input received
+			//default:
+			//	m.output += msg.String()
 		}
+
+	case TickMsg:
+		//m.output += "tick"
+		return m, tea.Batch(
+			m.GrepFetcher(),
+			tickEvery(),
+		)
+
 	case GrepMessage:
 		if msg.err == nil {
 			m.output = msg.result
+		} else {
+			m.output = msg.err.Error()
 		}
 	}
 
@@ -67,9 +78,10 @@ func (m Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	view := fmt.Sprintf("Result: %s \n", func() string {
-		if m.err == nil {
-			return m.output
+	view := fmt.Sprintf("Result: %s \n", m.output)
+	view += fmt.Sprintf("Error: %s \n", func() string {
+		if m.err != nil {
+			return m.err.Error()
 		}
 		return ""
 	}())
@@ -82,17 +94,27 @@ type GrepMessage struct {
 	err    error
 }
 
-func (m Model) GrepFetcher() GrepMessage {
-	command := exec.Command(m.inputBuffer.View())
-	output, err := command.Output()
-	if err == nil {
+func (m Model) GrepFetcher() tea.Cmd {
+	return func() tea.Msg {
+		command := exec.Command(m.inputBuffer.View())
+		output, err := command.Output()
+		if err == nil {
+			return GrepMessage{
+				result: string(output),
+				err:    nil,
+			}
+		}
 		return GrepMessage{
-			result: string(output),
-			err:    nil,
+			result: "",
+			err:    err,
 		}
 	}
-	return GrepMessage{
-		result: "",
-		err:    nil,
-	}
+}
+
+type TickMsg time.Time
+
+func tickEvery() tea.Cmd {
+	return tea.Every(time.Second, func(t time.Time) tea.Msg {
+		return TickMsg(t)
+	})
 }

64b17c595116033673856def2bc6b205b2bbc7fb
siggyweb <100291941+siggyweb@users.noreply.github.com>
2025-01-19 17:01:04 +0000

    code documentation + stripped command prefix

--- internal/tui/history.go
+++ internal/tui/history.go
@@ -1,16 +1,22 @@
 package tui
 
+import "strings"
+
+// HistoryModel is an interface where objects that implement the contract can provide backtracking capability to shell users.
 type HistoryModel interface {
 	GetPreviousCommand() string
 	GetNextCommand() string
 	AddCommand(string)
 }
 
+// History stores previously run shell commands which can be easily accessed, allowing users to backtrack effectively in their shell session.
 type History struct {
 	storedCommands []string
 	index          int
 }
 
+// GetPreviousCommand fetches the next stored command from the shell model. Fetching from beyond either end of the list
+// will return an empty string.
 func (h *History) GetPreviousCommand() string {
 	if len(h.storedCommands) == 0 {
 		return ""
@@ -21,6 +27,8 @@ func (h *History) GetPreviousCommand() string {
 	return ""
 }
 
+// GetNextCommand fetches the next stored command from the shell model. Fetching from beyond either end of the list
+// will return an empty string.
 func (h *History) GetNextCommand() string {
 	if len(h.storedCommands) == 0 {
 		return ""
@@ -34,10 +42,13 @@ func (h *History) GetNextCommand() string {
 	return ""
 }
 
+// AddCommand stores a previously run command in the shell model for future reuse. Only applied to commands which pass
+// validation
 func (h *History) AddCommand(command string) {
 	if command == "" {
 		return
 	}
-	h.storedCommands = append(h.storedCommands, command)
+	commandString, _ := strings.CutPrefix(command, "/bin/")
+	h.storedCommands = append(h.storedCommands, commandString)
 	h.index = len(h.storedCommands) - 1
 }
